d2211f81a32257ac0e8d5490057b725ce88153e1
MightyMaximus <jain.aiyush@gmail.com>
2020-08-11 10:34:34 -0400

    Challenge: Subrectangle Queries (Updated)

--- src/SubrectangleQueries.java
+++ src/SubrectangleQueries.java
@@ -2,31 +2,32 @@
  * <h1>Leet Code: </h1>
  * <h3>Challenge: Subrectangle Queries</h3>
  *
- * <h4>Runtime: 25ms - beats 85% of Java submissions</h4>
+ * <h4>Runtime: 23ms - beats 98% of Java submissions</h4>
  *
  * @author Aiyush Jain
- * @version 1.0
+ * @version 2.0
  * @since 2020-08-11
  * */
 public class SubrectangleQueries {
-    private int[][] rectangle;
-    private int rows, cols;
+    private int[][] rectangle, updates;
+    private int pos;
 
     public SubrectangleQueries(int[][] rectangle) {
         this.rectangle = rectangle;
-        this.rows = rectangle.length;
-        this.cols = rectangle.length;
+        this.updates = new int[500][5];
+        this.pos = 0;
     }
 
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-        for (int i = row1; i <= row2; i++) {
-            for (int j = col1; j <= col2; j++) {
-                rectangle[i][j] = newValue;
-            }
-        }
+        updates[pos++] = new int[] {row1, col1, row2, col2, newValue};
     }
 
     public int getValue(int row, int col) {
+        for (int i = pos - 1; i >= 0; i--) {
+            if (row >= updates[i][0] && row <= updates[i][2])
+                if (col >= updates[i][1] && col <= updates[i][3])
+                    return updates[i][4];
+        }
         return rectangle[row][col];
     }
 }
