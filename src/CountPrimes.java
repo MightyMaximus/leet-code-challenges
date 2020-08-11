92f4b82e7fe52ad0e1dfa7dfd46d923773435f7b
MightyMaximus <jain.aiyush@gmail.com>
2020-08-10 23:21:18 -0400

    Challenge: Count Primes (Updated)

--- src/CountPrimes.java
+++ src/CountPrimes.java
@@ -3,35 +3,31 @@
  * <h2>Section: Math</h2>
  * <h3>Challenge: Count Primes</h3>
  *
- * <h4>Runtime: 14ms - beats 75% of Java submissions</h4>
+ * <h4>Runtime: 5ms - beats 99% of Java submissions</h4>
  *
  * @author Aiyush Jain
- * @version 1.0
+ * @version 2.0
  * @since 2020-08-10
  * */
 public class CountPrimes {
     public int countPrimes(int n) {
-        int count = 0;
-        boolean[] isComposite = new boolean[n];
-        if (n > 2)
-            count += findComposites(2, n, isComposite);
-        for(int i = 3; i <= (n / 2); i += 2) {
-            count += findComposites(i, n, isComposite);
-        }
+        if (n < 3)
+            return 0;
 
-        for(int i = (n / 2) + 1; i < n; i++) {
-            if(!isComposite[i])
-                count++;
+        boolean[] isComposite = new boolean[n];
+        int count = n / 2;
+
+        for (int i = 3; (i * i) < n; i += 2) {
+            if (isComposite[i])
+                continue;
+
+            for (int j = (i * i); j < n; j += (i * 2)) {
+                if (!isComposite[j]) {
+                    count--;
+                    isComposite[j] = true;
+                }
+            }
         }
         return count;
-    }
-
-    private int findComposites(int i, int n, boolean[] isComposite) {
-        if(!isComposite[i]) {
-            for(int j = 2; (i * j) < n; j++)
-                isComposite[i * j] = true;
-            return 1;
-        }
-        return 0;
     }
 }
