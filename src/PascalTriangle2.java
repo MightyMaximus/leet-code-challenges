05c61e5224db96c9276528e7bd7555cb41ff6477
MightyMaximus <jain.aiyush@gmail.com>
2020-08-13 03:42:45 -0400

    Challenge: Pascal's Triangle II

---
+++ src/PascalTriangle2.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h1>Leet Code: August LeetCoding Challenge</h1>
+ * <h3>Challenge: Pascal's Triangle II</h3>
+ *
+ * <h4>Runtime: 1ms - beats 90% of Java submissions</h4>
+ *
+ * @author Aiyush Jain
+ * @version 1.0
+ * @since 2020-08-12
+ * */
+public class PascalTriangle2 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> triangle = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            int size = triangle.size();
+            for (int j = 0; j < size - 1; j++) {
+                triangle.add(triangle.get(0) + triangle.get(1));
+                triangle.remove(0);
+            }
+            triangle.add(1);
+        }
+        return triangle;
+    }
+}
